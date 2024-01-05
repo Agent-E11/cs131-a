0b430e71f932fc216e9c9f73cbaa565b53282e4e
Eamon <eamon.r.burns@gmail.com>
2024-01-05 15:38:27 -0800

    Complete lab 03 (actually this time)

---
+++ 03-number-guesser/03-number-guesser.cpp
@@ -0,0 +1,49 @@
+// 03-number-guesser.cpp
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	const int range_start = 1;
+	const int range_length = 10;
+	const int num_of_guesses = 3;
+
+	int range_end = range_start + (range_length - 1); // Calculate last number in range
+
+	int guessed_num;
+	int correct_num;
+
+	srand(time(NULL)); // Seed RNG
+
+	correct_num = rand() % range_length + range_start; // Get a random number in the range
+
+	// Print random number (for debugging)
+	cout << "Random number: " << correct_num << endl << endl;
+
+	// Prompt user
+	cout << "Guess a number between " << range_start
+		<< " and " << range_end
+		<< ", you have " << num_of_guesses
+		<< " guesses" << endl;
+
+	for (int i = 0; i < num_of_guesses; i++) {
+		cout << "Guess " << i + 1 << ": ";
+		cin >> guessed_num; // No error handling
+		cout << endl;
+
+		// Check if the number was correct
+		if (guessed_num == correct_num)
+		{
+			cout << "Correct!" << endl; // Congratulate and break out of loop
+			break;
+		}
+		else
+		{
+			cout << "Wrong, try again." << endl;
+		}
+	}
+}
