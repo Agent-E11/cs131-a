40d2a146680825f62cd130e78b5da4f156b8dfea
Eamon Burns (Laptop Powershell) <eamon.r.burns@gmail.com>
2024-03-05 23:50:07 -0800

    Add checks for y as a vowel, get string from user

--- 08-vowel-counter/08-vowel-counter.cpp
+++ 08-vowel-counter/08-vowel-counter.cpp
@@ -9,53 +9,68 @@
 /// </summary>
 /// <param name="word">A single word</param>
 /// <returns>The number of vowels in the word. Returns -1 if there was an error</returns>
-int count_vowels(std::string word) {
+static int count_vowels(std::string word) {
 	const char vowels[] = { 'a', 'e', 'i', 'o', 'u' };
 	int count = 0;
 	for (int i = 0; i < word.length(); i++) {
+		// If the character is a space, then it isn't a single word
 		if (std::isspace(word[i])) return -1;
+
+		// Find position of letter in vowels
 		const char* pos = std::find(std::begin(vowels), std::end(vowels), word[i]);
-		if (pos != std::end(vowels)) {
+
+		// If the letter is in vowels, increment the count
+		if (pos != std::end(vowels))
 			count++;
-		}
 	}
+
+	// If there are no other vowels, search for `y`s
+	if (count == 0)
+		for (int i = 0; i < word.length(); i++)
+			if (word[i] == 'y')
+				count += 1;
+
 	return count;
 }
 
-std::vector<std::string> split_words(std::string s) {
+/// <summary>
+/// Splits a string into a vector of words.
+/// A word is a group of characters surrounded by any amount of whitespace.
+/// Whitespace is defined by std::isspace().
+/// 
+/// Multiple spaces in a row will be treated the same as a single space
+/// </summary>
+/// <param name="s">String to be split</param>
+/// <returns>A vector of words</returns>
+static std::vector<std::string> split_words(std::string s) {
 	std::vector<std::string> words;
 	std::string word = "";
 
+	// Loop through the characters in the sentence
 	for (int i = 0; i < s.length(); i++) {
-		std::cout << "s[i] = `" << s[i] << "`\t";
 		if (std::isspace(s[i])) {
-			std::cout << "is space\t";
 			if (word.empty()) {
-				std::cout << "is empty\t" << std::endl;
+				// If the character is whitespace and the word is empty, ignore it
 				continue;
 			}
 			else {
-				std::cout << "no empty\t";
+				// If the character is whitespace and there is a word,
+				// add it to words and reset the word
 				words.push_back(word);
 				word = "";
 			}
 		}
 		else {
+			// If the character is not whitespace, add it to the word
 			word += s[i];
-			std::cout << "no space\t" << "w: " << word << "\t";
 		}
-		std::cout << std::endl;
 	}
-	// Add last word
+	// Add the final word (If it isn't empty)
 	if (!word.empty()) words.push_back(word);
 
 	return words;
 }
 
-/// <summary>
-/// Prints vector<string> to stdout
-/// </summary>
-/// <param name="v">Vector to be printed</param>
 static void print_vec(std::vector<std::string> v) {
 	std::cout << "[ ";
 	for (int i = 0; i < v.size(); i++)
@@ -64,15 +79,21 @@ static void print_vec(std::vector<std::string> v) {
 }
 
 int main() {
-	std::string s = "This is a    long test    of this thi  ng     ";
-	auto v = split_words(s);
+	// Get input
+	std::string input;
+	std::getline(std::cin, input);
 
-	print_vec(v);
+	// Split into words
+	std::vector<std::string> words = split_words(input);
 
+	print_vec(words);
+
+	// Sum up all vowels in all words
 	int count = 0;
-	for (int i = 0; i < v.size(); i++) {
-		int c = count_vowels(v[i]);
+	for (int i = 0; i < words.size(); i++) {
+		int c = count_vowels(words[i]);
 
+		// If there was an error, quit the program
 		if (c == -1)
 			exit(1);
 
