33e3f8bf852489b6a4f347629c09321df97f4de5
Agent-E11 <eamon.r.burns@gmail.com>
2024-03-06 19:42:10 -0800

    Create get_double function and add lots of comments

--- 24-03-06_classes/24-03-06_classes.cpp
+++ 24-03-06_classes/24-03-06_classes.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <vector>
 #include <format>
+#include <string>
+
+constexpr auto COULD_NOT_FIND_ERR = "Could not find account with name `{}`";
+constexpr auto INVALID_NUMBER_ERR = "Not a valid number";
 
 class BankAcct {
 public:
@@ -28,8 +32,9 @@ void display_accts(std::vector<BankAcct> accts);
 
 static int find_account(std::vector<BankAcct> accts, std::string name);
 
+bool get_double(double& n);
+
 int main() {
-	const char COULD_NOT_FIND_ERR[] = "Could not find account with name `{}`";
 
     std::cout.setf(std::ios::fixed);
     std::cout.setf(std::ios::showpoint);
@@ -50,13 +55,13 @@ int main() {
 	std::string msg = "";
 	do {
 		system("cls");
-		// display interface
+		// Display interface
 		std::cout << msg << std::endl << std::endl;
 		std::cout << "Account\tBalance\n";
 		std::cout << "------------------------\n";
 		display_accts(accts);
 		std::cout << "(A)dd\t(D)eposit\t(W)ithdraw\t(T)ransfer\t(R)emove\t(Q)uit\n\n> ";
-		// user input
+		// Get user input
 		std::cin >> choice;
 		choice = tolower(choice);
 
@@ -64,87 +69,116 @@ int main() {
 		std::string name2;
 		double balance;
 		double amount;
-		bool found;
+		bool success;
 		int pos;
 		BankAcct new_acct;
 
 		BankAcct *transfer_from_acct = nullptr;
 		BankAcct *transfer_to_acct = nullptr;
+
 		switch (choice) {
 		case 'a':
 			// Add account
 
+			// Get name
 			std::cout << "Name of new account: ";
 			std::cin >> new_acct.name;
-			std::cout << "Starting balance: $";
-			std::cin >> balance;
-			new_acct.deposit(balance);
 
+			// Get starting balance
+			std::cout << "Starting balance: $";
+			success = get_double(balance);
+			if (!success) {
+				msg = std::format(INVALID_NUMBER_ERR);
+				break;
+			}
+
+			// Add balance and add new_acct to accts
+			new_acct.deposit(balance);
 			accts.push_back(new_acct);
 
-			msg = std::format("Added acount `{}` with starting balance `${}`", new_acct.name, balance);
+			msg = std::format("Added acount `{}` with starting balance `${:.2f}`", new_acct.name, balance);
 			break;
+
 		case 'd':
+			// Deposit to account
+
+			// Get name
 			std::cout << "Name of account to deposit to: ";
 			std::cin >> name1;
 
+			// Find position of account
 			pos = find_account(accts, name1);
-
 			if (pos == -1) {
 				msg = std::format(COULD_NOT_FIND_ERR, name1);
 				break;
 			}
 
+			// Get amount
 			std::cout << "Amount to deposit: $";
-			std::cin >> amount;
-
+			success = get_double(amount);
+			if (!success) {
+				msg = std::format(INVALID_NUMBER_ERR);
+				break;
+			}
 			if (amount < 0) {
 				msg = "Cannot deposit a negative amount (use (W)ithdraw)";
 				break;
 			}
 
+			// Deposit amount
 			accts[pos].deposit(amount);
 
 			msg = std::format("Deposited `${:.2f}` to account `{}`", amount, name1);
-
 			break;
 
 		case 'w':
+			// Withdraw from account
+
+			// Get name
 			std::cout << "Name of account to withdraw from: ";
 			std::cin >> name1;
 
+			// Find position of account
 			pos = find_account(accts, name1);
-
 			if (pos == -1) {
 				msg = std::format(COULD_NOT_FIND_ERR, name1);
 				break;
 			}
 
+			// Get amount
 			std::cout << "Amount to withdraw: $";
-			std::cin >> amount;
-
+			success = get_double(amount);
+			if (!success) {
+				msg = std::format(INVALID_NUMBER_ERR);
+				break;
+			}
 			if (amount < 0) {
 				msg = "Cannot withdraw a negative amount (use (D)eposit)";
 				break;
 			}
 
+			// Deposit the inverse of amount (withdraw)
 			accts[pos].deposit(-amount);
 
 			msg = std::format("Withdrew `${:.2f}` from account `{}`", amount, name1);
 			break;
 
 		case 't':
+			// Transfer between accounts
+
+			// Get first name
 			std::cout << "Name of account to transfer from: ";
 			std::cin >> name1;
 
+			// Find position of account and get reference
 			pos = find_account(accts, name1);
-
 			if (pos == -1) {
 				msg = std::format(COULD_NOT_FIND_ERR, name1);
 				break;
 			}
 			transfer_from_acct = &accts[pos];
 
+			// Get second name
 			std::cout << "Name of account to transfer to: ";
 			std::cin >> name2;
 
@@ -153,41 +187,53 @@ int main() {
 				break;
 			}
 
+			// Find position of account and get reference
 			pos = find_account(accts, name2);
-
 			if (pos == -1) {
 				msg = std::format(COULD_NOT_FIND_ERR, name2);
 				break;
 			}
 			transfer_to_acct = &accts[pos];
 
+			// Get amount
 			std::cout << std::format("Amount to transfer from `{}` to `{}`: $", name1, name2);
-			std::cin >> amount;
+			success = get_double(amount);
+			if (!success) {
+				msg = std::format(INVALID_NUMBER_ERR);
+				break;
+			}
 
+			// Subtract from first account, add to second account
 			transfer_from_acct->deposit(-amount);
 			transfer_to_acct->deposit(amount);
 
 			msg = std::format("Transferred `${:.2f}` from `{}` to `{}`", amount, name1, name2);
-
 			break;
 
 		case 'r':
+			// Remove account
+
+			// Get name
 			std::cout << "Name of account to remove: ";
 			std::cin >> name1;
 
+			// Find position of account
 			pos = find_account(accts, name1);
-			
+
 			if (pos != -1) {
+				// If it is there, remove account
 				accts.erase(accts.begin() + pos);
 				msg = std::format("Removed acount `{}`", name1);
 			}
 			else {
+				// If it is not there, set msg to an error
 				msg = std::format(COULD_NOT_FIND_ERR, name1);
 			}
 			break;
 
 		case 'q':
 			// Quit program
+
 			std::cout << "Quitting... \n";
 			break;
 
@@ -208,6 +254,28 @@ static int find_account(std::vector<BankAcct> accts, std::string name) {
 		if (accts[i].name == name) return i;
 	}
 	return -1;
+}
+
+/// <summary>
+/// Get a double using std::cin
+/// </summary>
+/// <param name="n">Reference to be overwritten with the new double. If there is an error, it will not be changed</param>
+/// <returns>Returns the success value, true if success, false if fail</returns>
+bool get_double(double& n) {
+	// Get input
+	std::string input;
+	std::cin >> input;
+
+	try {
+		// Try to convert to double
+		n = std::stod(input);
+	}
+	catch (std::exception& e) {
+		// If there was an error, return false
+		return false;
+	}
+	// If there was no error, return true
+	return true;
 }
 
 void display_accts(std::vector<BankAcct> accts) {
